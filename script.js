bf18a2eed9472c077265b5e3fa6425056686d85f
rcfgama <rcfgama@gmail.com>
2020-05-28 14:09:01 -0300

    Atualizações realizadas
    - Agora o Lay-out é responsivo com Bootstrap.
    - Agora na lista de copras também tem uma busca de produtos.
    - Agora o site é todo dinamico com PHP.
    - Todos os valores tiveram a formatação corrigida.

--- script.js
+++ script.js
@@ -1,6 +1,93 @@
-// Arquivo JavaScript com função para confirmar a exclusão de produtos da lista na página Fast Shoper. Desenvolvida por Ronaldo Gama - versão 1.3
+// Arquivo JavaScript com funçôes para envio de IDs para outras páginas processarem na página Fast Shopper. 
+// Desenvolvida por Ronaldo Gama - versão 0.4
+function editera(altid) {
+	
+	var formaltera = document.createElement("form");
+		formaltera.action = "?b=altera";
+		formaltera.method = "post";
+
+		var inputera = document.createElement("input");
+		inputera.type = "hidden";
+		inputera.value = altid;
+		inputera.name = "id";
+		formaltera.appendChild(inputera);
+
+		document.body.appendChild(formaltera);
+
+		formaltera.submit();	
+
+}
+
 function verifica(recid) {
 	if(confirm("Tem certeza que deseja excluir permanentemente este item?")) {
-		window.location="deleta.php?idexc=" + recid
+	
+	var formulario = document.createElement("form");
+		formulario.action = "?b=deleta";
+		formulario.method = "post";
+
+		var inputId = document.createElement("input");
+		inputId.type = "hidden";
+		inputId.value = recid;
+		inputId.name = "id";
+		formulario.appendChild(inputId);
+
+		document.body.appendChild(formulario);
+
+		formulario.submit();	
+
 	}
+}
+
+function update(updid) {
+	
+	var formupdate = document.createElement("form");
+		formupdate.action = "?b=upall";
+		formupdate.method = "post";
+
+		var inpudate = document.createElement("input");
+		inpudate.type = "hidden";
+		inpudate.value = updid;
+		inpudate.name = "id";
+		formupdate.appendChild(inpudate);
+
+		document.body.appendChild(formupdate);
+
+		formupdate.submit();	
+
+}
+
+function downera(dowid) {
+	
+	var formdown = document.createElement("form");
+		formdown.action = "?b=down";
+		formdown.method = "post";
+
+		var inpudown = document.createElement("input");
+		inpudown.type = "hidden";
+		inpudown.value = dowid;
+		inpudown.name = "id";
+		formdown.appendChild(inpudown);
+
+		document.body.appendChild(formdown);
+
+		formdown.submit();	
+
+}
+
+function upera(upeid) {
+	
+	var formera = document.createElement("form");
+		formera.action = "?b=upcar";
+		formera.method = "post";
+
+		var inpera = document.createElement("input");
+		inpera.type = "hidden";
+		inpera.value = upeid;
+		inpera.name = "id";
+		formera.appendChild(inpera);
+
+		document.body.appendChild(formera);
+
+		formera.submit();	
+
 }
